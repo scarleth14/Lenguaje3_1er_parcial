43ecdc1425180c38b093ca50851db5d6f3f4eee9
SCARLETH ABIGAIL BAQUEDANO PADILLA <sabaquedano@unah.hn>
2021-09-13 23:05:16 -0600

    Manipulación de cadenas de texto

---
+++ Primer_parcial/Ejercicio1/Funciones_de_texto.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Ejercicio1
+{
+    partial class Funciones_de_texto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_reemplazar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_minuscula = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_mayusculas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_rango = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_ultimo_caracter = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_primer_caracter = new System.Windows.Forms.TextBox();
+            this.txt_longitud = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_ejecutar = new System.Windows.Forms.Button();
+            this.txt_cadena = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(386, 196);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 17);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Reemplazar";
+            // 
+            // txt_reemplazar
+            // 
+            this.txt_reemplazar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_reemplazar.Location = new System.Drawing.Point(525, 189);
+            this.txt_reemplazar.Name = "txt_reemplazar";
+            this.txt_reemplazar.Size = new System.Drawing.Size(138, 23);
+            this.txt_reemplazar.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(386, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 17);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Minuscula";
+            // 
+            // txt_minuscula
+            // 
+            this.txt_minuscula.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_minuscula.Location = new System.Drawing.Point(525, 144);
+            this.txt_minuscula.Name = "txt_minuscula";
+            this.txt_minuscula.Size = new System.Drawing.Size(138, 23);
+            this.txt_minuscula.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(386, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 17);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Mayusculas ";
+            // 
+            // txt_mayusculas
+            // 
+            this.txt_mayusculas.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_mayusculas.Location = new System.Drawing.Point(525, 100);
+            this.txt_mayusculas.Name = "txt_mayusculas";
+            this.txt_mayusculas.Size = new System.Drawing.Size(138, 23);
+            this.txt_mayusculas.TabIndex = 28;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Rango";
+            // 
+            // txt_rango
+            // 
+            this.txt_rango.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_rango.Location = new System.Drawing.Point(176, 234);
+            this.txt_rango.Name = "txt_rango";
+            this.txt_rango.Size = new System.Drawing.Size(138, 23);
+            this.txt_rango.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 17);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Ultimo caracter";
+            // 
+            // txt_ultimo_caracter
+            // 
+            this.txt_ultimo_caracter.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ultimo_caracter.Location = new System.Drawing.Point(176, 189);
+            this.txt_ultimo_caracter.Name = "txt_ultimo_caracter";
+            this.txt_ultimo_caracter.Size = new System.Drawing.Size(138, 23);
+            this.txt_ultimo_caracter.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Primer caracter";
+            // 
+            // txt_primer_caracter
+            // 
+            this.txt_primer_caracter.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_primer_caracter.Location = new System.Drawing.Point(176, 145);
+            this.txt_primer_caracter.Name = "txt_primer_caracter";
+            this.txt_primer_caracter.Size = new System.Drawing.Size(138, 23);
+            this.txt_primer_caracter.TabIndex = 22;
+            // 
+            // txt_longitud
+            // 
+            this.txt_longitud.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_longitud.Location = new System.Drawing.Point(176, 100);
+            this.txt_longitud.Name = "txt_longitud";
+            this.txt_longitud.Size = new System.Drawing.Size(138, 23);
+            this.txt_longitud.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Longitud";
+            // 
+            // btn_ejecutar
+            // 
+            this.btn_ejecutar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ejecutar.Location = new System.Drawing.Point(541, 51);
+            this.btn_ejecutar.Name = "btn_ejecutar";
+            this.btn_ejecutar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ejecutar.TabIndex = 19;
+            this.btn_ejecutar.Text = "Ejecutar";
+            this.btn_ejecutar.UseVisualStyleBackColor = true;
+            // 
+            // txt_cadena
+            // 
+            this.txt_cadena.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_cadena.Location = new System.Drawing.Point(176, 53);
+            this.txt_cadena.Name = "txt_cadena";
+            this.txt_cadena.Size = new System.Drawing.Size(290, 23);
+            this.txt_cadena.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Cadena de texto";
+            // 
+            // Funciones_de_texto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 311);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_reemplazar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_minuscula);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_mayusculas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_rango);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_ultimo_caracter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_primer_caracter);
+            this.Controls.Add(this.txt_longitud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_ejecutar);
+            this.Controls.Add(this.txt_cadena);
+            this.Controls.Add(this.label1);
+            this.Name = "Funciones_de_texto";
+            this.Text = "Funciones_de_texto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_reemplazar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_minuscula;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_mayusculas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_rango;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_ultimo_caracter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_primer_caracter;
+        private System.Windows.Forms.TextBox txt_longitud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_ejecutar;
+        private System.Windows.Forms.TextBox txt_cadena;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Primer_parcial/Ejercicio1/Funciones_de_texto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Funciones_de_texto : Form
+    {
+        public Funciones_de_texto()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ejecutar_Click(object sender, EventArgs e)
+        {
+            string cadena = txt_cadena.Text;
+
+            //Obtiene la longitud de la cadena
+            txt_longitud.Text = cadena.Length.ToString();
+
+            //Obtiene el primer elemento de la cadena
+            txt_primer_caracter.Text = cadena.Substring(0, 1);
+
+            //Obtiene el ultimo elemento
+            txt_ultimo_caracter.Text = cadena.Substring(cadena.Length - 1, 1);
+
+            //Obtiene un rango de caracteres
+            txt_rango.Text = cadena.Substring(5, 15);
+
+            //Convertir a mayusculas
+            txt_mayusculas.Text = cadena.ToUpper();
+
+            //Convertir a minusculas
+            txt_minuscula.Text = cadena.ToLower();
+
+            //Reemplazar caracteres
+            txt_reemplazar.Text = cadena.Replace("a", "b");
+
+        }
+    }
+}
